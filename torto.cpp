5ed84e14490765679bb0cc60ca8e093d6138ae99
André de la Rocha <47720370+adlroc@users.noreply.github.com>
2019-03-18 00:39:37 -0300

    optimizations

--- torto.cpp
+++ torto.cpp
@@ -17,18 +17,15 @@
 #define PAD_COL 1
 #define PAD_ROW 2
 
-// Defined by bitmask type. It can be trivially increased by using
-// multiple array elements, but 64 seems good enough for our needs.
 #define MAX_WORDS 64
 
 const char *words[MAX_WORDS];
-int len[MAX_WORDS];
 
 // board characters
-char board[NUM_ROW][NUM_COL+PAD_COL];
-// bitmask to mark simultaneous covering of a site by multiple words
-unsigned long long bitmask[NUM_ROW][NUM_COL+PAD_COL];
-// breadcrumbs to enforce "no diagonal crossing" constraint
+int board[NUM_ROW][NUM_COL+PAD_COL];
+// counts the number of words simultaneously covering a board site
+int occupancy[NUM_ROW][NUM_COL+PAD_COL];
+// tracks the steps of a word being placed on the board
 int crumbs[MAX_WORDS][NUM_ROW+PAD_ROW][NUM_COL+PAD_COL];
 
 // all unique solutions including mirrored configurations
@@ -39,8 +36,6 @@ unsigned long long dsol_count;
 unsigned long long esol_count;
 // essentially unique and determinate solutions
 unsigned long long edsol_count;
-// number of recursive calls
-unsigned long long call_count;
 
 // Set to filter out duplicate solutions.
 std::unordered_set<std::string> unq_sols;
@@ -61,53 +56,53 @@ void output_solution();
 
 void rec_torto(int n, int w, int l, int i, int j)
 {
-	++call_count;
 	crumbs[w][i][j] = l+2;
 	board[i][j] = words[w][l];
-	bitmask[i][j] |= 1<<w;
+	++occupancy[i][j];
 
-	if (l < len[w]-1) {
-		// next char in word
-		char c = words[w][l+1];
+	// next char in word
+	char c = words[w][l+1];
+
+	if (c) {
 		// north
-		if (i > 0 && (!board[i-1][j] || board[i-1][j] == c)
-				&& !(bitmask[i-1][j] & (1<<w)))
+		if (i > 0 && (!occupancy[i-1][j] || board[i-1][j] == c)
+				&& !crumbs[w][i-1][j])
 			rec_torto(n, w, l+1, i-1, j);
 		if (j < NUM_COL-1) {
 			// east
-			if ((!board[i][j+1] || board[i][j+1] == c)
-					&& !(bitmask[i][j+1] & (1<<w)))
+			if ((!occupancy[i][j+1] || board[i][j+1] == c)
+					&& !crumbs[w][i][j+1])
 				rec_torto(n, w, l+1, i, j+1);
 			// northeast
-			if (i > 0 && (!board[i-1][j+1] || board[i-1][j+1] == c)
-					&& !(bitmask[i-1][j+1] & (1<<w))
+			if (i > 0 && (!occupancy[i-1][j+1] || board[i-1][j+1] == c)
+					&& !crumbs[w][i-1][j+1]
 					&& abs(crumbs[w][i-1][j] - crumbs[w][i][j+1]) != 1)
 				rec_torto(n, w, l+1, i-1, j+1);
 			// southeast
-			if (i < NUM_ROW-1 && (!board[i+1][j+1] || board[i+1][j+1] == c)
-					&& !(bitmask[i+1][j+1] & (1<<w))
+			if (i < NUM_ROW-1 && (!occupancy[i+1][j+1] || board[i+1][j+1] == c)
+					&& !crumbs[w][i+1][j+1]
 					&& abs(crumbs[w][i+1][j] - crumbs[w][i][j+1]) != 1)
 				rec_torto(n, w, l+1, i+1, j+1);
 		}
 		if (j > 0) {
 			// west
-			if ((!board[i][j-1] || board[i][j-1] == c)
-					&& !(bitmask[i][j-1] & (1<<w)))
+			if ((!occupancy[i][j-1] || board[i][j-1] == c)
+					&& !crumbs[w][i][j-1])
 				rec_torto(n, w, l+1, i, j-1);
 			// northwest
-			if (i > 0 && (!board[i-1][j-1] || board[i-1][j-1] == c)
-					&& !(bitmask[i-1][j-1] & (1<<w))
+			if (i > 0 && (!occupancy[i-1][j-1] || board[i-1][j-1] == c)
+					&& !crumbs[w][i-1][j-1]
 					&& abs(crumbs[w][i-1][j] - crumbs[w][i][j-1]) != 1)
 				rec_torto(n, w, l+1, i-1, j-1);
 			// southwest
-			if (i < NUM_ROW-1 && (!board[i+1][j-1] || board[i+1][j-1] == c)
-					&& !(bitmask[i+1][j-1] & (1<<w))
+			if (i < NUM_ROW-1 && (!occupancy[i+1][j-1] || board[i+1][j-1] == c)
+					&& !crumbs[w][i+1][j-1]
 					&& abs(crumbs[w][i+1][j] - crumbs[w][i][j-1]) != 1)
 				rec_torto(n, w, l+1, i+1, j-1);
 		}
 		// south
-		if (i < NUM_ROW-1 && (!board[i+1][j] || board[i+1][j] == c)
-				&& !(bitmask[i+1][j] & (1<<w)))
+		if (i < NUM_ROW-1 && (!occupancy[i+1][j] || board[i+1][j] == c)
+				&& !crumbs[w][i+1][j])
 			rec_torto(n, w, l+1, i+1, j);
 	} else {
 		// last char
@@ -116,12 +111,11 @@ void rec_torto(int n, int w, int l, int i, int j)
 			output_solution();
 		} else {
 			// next word
-			char c = words[w+1][0];
+			char nc = words[w+1][0];
 			for (int y = 0; y < NUM_ROW; ++y) {
 				for (int x = 0; x < NUM_COL; ++x) {
 					static unsigned int h;
-					if ((!board[y][x] || board[y][x] == c)
-							&& !(bitmask[y][x] & (1<<(w+1)))
+					if ((!occupancy[y][x] || board[y][x] == nc)
 							&& (w > 0 || (h = h * 1103515245U + 12345U) % par_cnt == par_idx))
 						rec_torto(n, w+1, 0, y, x);
 				}
@@ -129,8 +123,7 @@ void rec_torto(int n, int w, int l, int i, int j)
 		}
 	}
 	// undo changes
-	bitmask[i][j] &= ~(1<<w);
-	if (!bitmask[i][j]) board[i][j] = 0;
+	--occupancy[i][j];
 	crumbs[w][i][j] = 0;
 }
 
@@ -149,7 +142,7 @@ bool is_determinate()
 {
 	for (int i = 0; i < NUM_ROW; ++i)
 		for (int j = 0; j < NUM_COL; ++j)
-			if (!board[i][j])
+			if (!occupancy[i][j])
 				return false;
 	return true;
 }
@@ -157,9 +150,13 @@ bool is_determinate()
 std::string board_key(bool flipy, bool flipx)
 {
 	std::string key;
-	for (int i = 0; i < NUM_ROW; ++i)
-		for (int j = 0; j < NUM_COL; ++j)
-			key += board[flipy ? NUM_ROW-1-i : i][flipx ? NUM_COL-1-j : j];
+	for (int i = 0; i < NUM_ROW; ++i) {
+		for (int j = 0; j < NUM_COL; ++j) {
+			int y = flipy ? NUM_ROW-1-i : i;
+			int x = flipx ? NUM_COL-1-j : j;
+			key += (occupancy[y][x] ? static_cast<char>(board[y][x]) : '.');
+		}
+	}
 	return key;
 }
 
@@ -167,8 +164,9 @@ void output_board(bool flipy, bool flipx)
 {
 	for (int i = 0; i < NUM_ROW; ++i) {
 		for (int j = 0; j < NUM_COL; ++j) {
-			char c = board[flipy ? NUM_ROW-1-i : i][flipx ? NUM_COL-1-j : j];
-			std::cout << (c ? c : '.');
+			int y = flipy ? NUM_ROW-1-i : i;
+			int x = flipx ? NUM_COL-1-j : j;
+			std::cout << (occupancy[y][x] ? static_cast<char>(board[y][x]) : '.');
 		}
 		if (!single_line) std::cout << "\n";
 	}
@@ -268,11 +266,8 @@ int main(int argc, char *argv[])
 	if (!disable_pre_sort)
 		std::sort(wordvec.begin(), wordvec.end(), sorter);
 
-	for (int i = 0; i < num_words; ++i) {
+	for (int i = 0; i < num_words; ++i)
 		words[i] = wordvec[i].c_str();
-		len[i] = static_cast<int>(wordvec[i].length());
-		if (len[i] < 1) return -1;
-	}
 
 	torto(num_words);
 
@@ -281,7 +276,6 @@ int main(int argc, char *argv[])
 				  << "  (" << dsol_count << " determinate)\n";
 		std::cerr << "Essentially unique solutions: " << esol_count
 				  << "  (" << edsol_count << " determinate)\n";
-		std::cerr << "Call count: " << call_count << "\n";
 	}
 	return 0;
 }
